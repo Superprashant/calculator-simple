7430bc09388edffb6ffc40b448f21dcf19e47ce0
Prashant Birajdar <92636189+Superprashant@users.noreply.github.com>
2021-11-20 17:58:58 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,17 @@
+/*Write a program in C++ to display various type or arithmetic operation using mixed data type.*/
+#include <iostream>
+using namespace std;
+
+int main() {
+  double x, y;
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "Type another number: ";
+  cin >> y;
+  cout << "Sum is: " << x+y<<endl;
+  cout << "subtraction is: "<<x-y<<endl;
+cout << "multiplication is: "<<x*y<<endl;
+cout << "devision is: "<<x/y<<endl;
+
+  return 0;
+}
